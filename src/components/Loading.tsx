b13bd3fd96377870e4c79981c70817372381c4b4
Daniel Pan <daniel.panhead1@gmail.com>
2021-02-05 15:58:46 -0500

    added login interface

---
+++ src/components/Loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className="center-screen">
+      <div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
+    </div>
+  )
+}
+
+export default Loading
